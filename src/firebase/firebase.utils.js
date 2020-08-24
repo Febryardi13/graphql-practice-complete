47769ea8b80e1db3af4450c1930db45bda793753
Febryardi13 <febryardiwork@gmail.com>
2020-08-24 21:12:24 +0700

    complete-graphql-lessson

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+    apiKey: "AIzaSyDzAzJqbDSjNZvUZkCAQo5_pMX6vKNUiCQ",
+    authDomain: "juragankaos-baff6.firebaseapp.com",
+    databaseURL: "https://juragankaos-baff6.firebaseio.com",
+    projectId: "juragankaos-baff6",
+    storageBucket: "juragankaos-baff6.appspot.com",
+    messagingSenderId: "153378941082",
+    appId: "1:153378941082:web:16f804df0a3b9598585723",
+    measurementId: "G-1ZDDPYL96L"
 };
 
 firebase.initializeApp(config);
